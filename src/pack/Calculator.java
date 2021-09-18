f0ceba9b085cd2838ce961da9ff5ddbce6204b9f
Eric Vara <vara.ada91@gmail.com>
2021-09-18 07:13:18 -0500

    // condensed code

--- src/pack/Calculator.java
+++ src/pack/Calculator.java
@@ -4,46 +4,42 @@ public class Calculator {
     /*
         This program generates a functional calculator.
         Author: Eric
-        Date: 2/19/2021
+        Date: 2/19/2021111111
 
         // TODO: Add scanner and user friendly functions
-        // TODO: Create GUI Toolss
+        // TODO: Create GUI Toolssssssss
     */
 
-    public Calculator() {}
-
     // Add method
-    public int add(int a, int b) {
+    public static int add(int a, int b) {
         return a + b;
     }
 
     // Subtract method
-    public int subtract(int a, int b) {
+    public static int subtract(int a, int b) {
         return a - b;
     }
 
     // Multiply method
-    public int multiply(int a, int b) {
+    public static int multiply(int a, int b) {
         return a * b;
     }
 
     // Divide method
-    public int divide(int a, int b) {
+    public static int divide(int a, int b) {
         return a / b;
     }
 
     // Remainder
-    public int module(int a, int b) {
+    public static int module(int a, int b) {
         return a % b;
     }
 
     public static void main(String[] args) {
-        Calculator myCalculator = new Calculator();
-
-        System.out.println("5 + 7 = " + myCalculator.add(5, 7));
-        System.out.println("45 - 11 = " + myCalculator.subtract(45, 11));
-        System.out.println("2 * 12 = " + myCalculator.multiply(2, 12));
-        System.out.println("30 / 3 = " + myCalculator.divide(30, 3));
-        System.out.println("3 % 2 = " + myCalculator.module(3, 2));
+        System.out.println("5 + 7 = " + add(5, 7));
+        System.out.println("45 - 11 = " + subtract(45, 11));
+        System.out.println("2 * 12 = " + multiply(2, 12));
+        System.out.println("30 / 3 = " + divide(30, 3));
+        System.out.println("3 % 2 = " + module(3, 2));
     }
 }
